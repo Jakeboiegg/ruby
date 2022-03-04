9843d8a6667e64bf0be2928199161d39948bbc21
Jakeboiegg <100768121+Jakeboiegg@users.noreply.github.com>
2022-03-04 09:11:55 +0800

    Add files via upload

---
+++ math.rb
@@ -0,0 +1,13 @@
+puts "Is your math tuitor good? (y/n)"
+input = gets.chomp
+
+if input == "y"
+    puts "Much cool"
+
+elsif input == "n"
+    puts "Much sad"
+
+else
+    puts "Hello I asked you to write y or n you ƒπ¥¨ç∂≈¬®˙∫!!!!"
+
+end
